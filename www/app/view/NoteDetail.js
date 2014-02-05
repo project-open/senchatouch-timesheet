a7371a09da4d805b963ba132ab146bded82da4e4
Frank Bergmann <frank.bergmann@project-open.com>
2014-02-05 15:47:06 +0000

    - Senchatouch TS:   Fixed issue with unclean destruction of buttons with "id" property

--- www/app/view/NoteDetail.js
+++ www/app/view/NoteDetail.js
@@ -45,12 +45,73 @@ Ext.define('PO.view.NoteDetail', {
                 xtype: 'button',
                 text: 'Save',
                 ui: 'confirm',
-		id: 'noteSaveButton'
+		itemID: 'noteSaveButton',
+		handler: function(button, event) {
+		    console.log('NoteListController: Button "Save" pressed');
+		    
+		    // Save the form values to the record.
+		    // The record was set by the NoteNavigationController
+		    var noteStore = Ext.data.StoreManager.lookup('NoteStore');
+		    var form = button.up('formpanel');
+		    var values = form.getValues();
+		    var rec = form.getRecord();
+		    
+		    // Check if we are about to create a new note
+		    if (typeof rec === "undefined" || rec == null) {
+			// Create a new note and set values
+			rec = Ext.ModelManager.create(values, 'PO.model.Note');
+			rec.set(values);
+		    }
+		    rec.save({                // Save the model - generates PUT or POST to REST backend
+			success: function(record, operation) {
+			    console.log('Successfully updated im_note object');
+			    // Retrieve the new server-side Oid (object ID) of the new note and store into the model
+			    if (record.get('id') == '') {
+				var restResponseText = operation._response.responseText;
+				var restResponseObject = Ext.JSON.decode(restResponseText);
+				var restOid = '' + restResponseObject.data[0].rest_oid; // new object_id as string
+				record.set('id', restOid+'');                      // set the ID of the note_id
+				noteStore.add(rec);                                // Add the note to the store.
+			    }
+			},
+			failure: function(record, operation) {
+			    Ext.Msg.alert('Failed', operation.request.scope.reader.jsonData["message"], Ext.emptyFn);
+			}
+		    });
+		    
+		    // Return to the list of notes page
+		    var navView = button.up('noteNavigationView');
+		    navView.pop();
+		}
+		
             }, {
                 xtype: 'button',
                 text: 'Delete',
                 ui: 'decline',
-		id: 'noteDeleteButton'
+		itemID: 'noteDeleteButton',
+		handler: function(button, event) {
+		    console.log('NoteListController: Button "Delete" pressed');
+		    
+		    // Save the form values to the record.
+		    // The record was set by the NoteNavigationController
+		    var noteStore = Ext.data.StoreManager.lookup('NoteStore');
+		    var form = button.up('formpanel');
+		    var values = form.getValues();
+		    var rec = form.getRecord();
+		    
+		    // Did we create a completely new note?
+		    if (typeof rec === "undefined" || rec == null) {
+			return;	            // The record was new - no need to delete then...
+		    }
+		    
+		    noteStore.remove(rec);      // Remove the note from the store
+		    rec.erase();                // Generates DELETE call to REST backend
+		    
+		    // Return to the list of notes page
+		    var navView = button.up('noteNavigationView');
+		    navView.pop();
+		}
+
             }
         ]
     }
