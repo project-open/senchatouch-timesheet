e21d555c59c1f344d7695b1d5dd9386b5569454b
Frank Bergmann <frank.bergmann@project-open.com>
2015-06-12 07:26:44 +0000

    - Changed .org to .com or .net, depending on the situation

--- www/app/store/HourStore.js
+++ www/app/store/HourStore.js
@@ -4,7 +4,7 @@
  * Copyright (c) 2011 - 2014 ]project-open[ Business Solutions, S.L.
  * This file may be used under the terms of the GNU General Public 
  * License version 3.0 or alternatively unter the terms of the ]po[ 
- * FL or CL license as specified in www.project-open.org/en/license.
+ * FL or CL license as specified in www.project-open.com/en/license.
  */
 
 /*
--- www/index.tcl
+++ www/index.tcl
@@ -3,11 +3,11 @@
 # Copyright (c) 2011 - 2014 ]project-open[ Business Solutions, S.L.
 # This file may be used under the terms of the GNU General Public 
 # License version 3.0 or alternatively unter the terms of the ]po[ 
-# FL or CL license as specified in www.project-open.org/en/license.
+# FL or CL license as specified in www.project-open.com/en/license.
 #
 
 ad_page_contract {
-    @author frank.bergmann@ticket-open.com
+    @author frank.bergmann@project-open.com
 } {
 }
 
