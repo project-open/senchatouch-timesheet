cd02c72ba9bdbff65de934464b2012e091bc5150
Frank Bergmann <frank.bergmann@project-open.com>
2015-12-09 15:07:01 +0000

    - Sencha Touch Timesheet:   Added a splash/advertizing screen for Home

---
+++ tcl/senchatouch-timesheet-procs.tcl
@@ -0,0 +1,38 @@
+
+
+ad_proc -public senchatouch_timesheet_home_component {} {
+    Home component advertizing Mobile Timesheet
+} {
+    return "
+
+<table>
+<tr valign=top>
+<td>    
+    <a href='/senchatouch-timesheet/'><img 
+    src='/senchatouch-timesheet/resources/screenshot/mobile_timesheet.318x480.png'
+    ></a>
+</td>
+<td>
+    <h1>Experimental</h1>
+    <p>
+    <a href='/senchatouch-timesheet/'>\]po\[ Mobile Timesheet</a> 
+    is a timesheet logging application for mobile devices
+    written in HTML5 and available as part of every \]po\[ server.
+    We have included this application in 
+    <nobr>\]project-open\[</nobr> V5.0 as a preview in order to 
+    show you what is possible, while the mobile app may not yet be
+    ready for production use.
+    </p><p>
+    We will continue to develop this mobile app and provide 
+    you with a more mature system in the next release of \]po\[.
+    </p><p>
+    Please visit our
+    <a href='http://www.project-open.com/en/tutorial-building-sencha-touch-applications'
+    target='_blank'>Mobile HTML5 Development Tutorial</a>. 
+    You can modify this app by editing the the files here on this server
+    in ~projop/packages/senchatouch-timesheet/www/app.js
+</td>
+</tr>
+</table>
+"
+}
